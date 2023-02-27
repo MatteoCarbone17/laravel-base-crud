9370060ac9bafcb9dab182ea0c694f55bc576a2e
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-27 15:45:31 +0100

    add comments

--- resources/views/admin/books/show.blade.php
+++ resources/views/admin/books/show.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col">
+        {{-- <div class="row">
+            <div class="col">        sezione messaggio di errore
                 <div class=" m-4">
                     @if (session('message'))
                         <div class=" alert alert{{ session('classMessage') }}">
@@ -12,7 +12,7 @@
                     @endif
                 </div>
             </div>
-        </div>
+        </div> --}}
         <div class="row">
             <div class="col">
                 <div class="card  rounded-4 p-5">
@@ -24,33 +24,34 @@
                             <i class="fa-solid fa-envelopes-bulk"></i>
                         </span>
                         {{-- <p>
-                            <a class="dropdown-item" href="{{ url('profile') }}"> {{ Auth::user()['name'] }}</a>
+                            <a class="dropdown-item" href="{{ url('profile') }}"> {{ Auth::user()['name'] }}</a>   collegamento  al profilo dell'utente loggato
                         </p> --}}
                     </div>
                     <div class="card-body  rounded-4 text-center">
                       <h5 class="card-title mt-2 mb-2">{{ $book->title }}</h5>
                         <div class="card-img mt-2 mb-2">
-                            {{-- <img src="{{ asset('storage/imgs/'. $book->cover_image) }}" class="img-fluid" alt=""> --}}
+                            {{-- <img src="{{ asset('storage/imgs/'. $book->cover_image) }}" class="img-fluid" alt=""> --}}    cover libro
                         </div>
                         <p class="card-text p-3">{{ $book->content }}</p>
                         <div class="card-footer  rounded-4 p-3">
                             <span class="d-block">Data di pubblicazione : {{ $book->publication_date}} </span>
                         </div>
-                        <div class="col">
-                            <div class="mt-3">
-                                <a class="btn btn-warning" href="{{ route('admin.projects.edit', $project->id) }}"> Edit
+                        {{-- <div class="col">
+                            <div class="mt-3">      collegamento con bottoni per edit ,destroy, delete 
+
+                                <a class="btn btn-warning" href="{{ route('admin.projects.edit', $book->id) }}"> Edit
                                     <i class="fa-solid fa-edit"></i> </a>
                                 <form class="d-inline-block delete double-confirm"
-                                    action="{{ route('admin.projects.destroy', $project->id) }}" method="POST">
+                                    action="{{ route('admin.projects.destroy', $book->id) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
                                     <button class="btn btn-danger">Delete <i class="fa-solid fa-trash"></i></button>
                                 </form>
                             </div>
-                        </div>
-                        <div class="row mt-5">
+                        </div> --}}
+                        {{-- <div class="row mt-5">
                           <div class="col-4">
-                            @if (isset($previousProject->id))
+                            @if (isset($previousProject->id))           bottoni per per avanti e indietro 
                                 <a class="btn btn-outline-primary mt-3"
                                     href="{{ route('admin.projects.show', $previousProject->id) }}">Previous Page</a>
                             @else
@@ -68,7 +69,7 @@
                             @else
                                 <a class="btn btn-outline-primary disabled mt-3" href="">End Next Page</a>
                             @endif
-                        </div>
+                        </div> --}}
                         </div>
                     </div>
                 </div>
@@ -76,6 +77,6 @@
         </div>
     </div>
 @endsection
-{{-- @section('script')
+{{-- @section('script')   script èer la delete 
     @vite('resources/js/deleteButton.js')
 @endsection --}}

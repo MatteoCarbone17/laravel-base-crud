d2d9e995b913841342cb7f87ca2266728f0fae2f
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-27 12:44:59 +0100

    create index, add scaffolding

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function (){
-    Route::resource('/books', AdminBookController::class);
-});
-
-Route::get('/dashboard', function () {
+ Route::get('/dashboard', function () {    
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function (){
+    Route::resource('/books', AdminBookController::class);
+});
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
